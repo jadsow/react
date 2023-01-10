79c55f234fbcbe500f072fbefc737afc6e2d77d3
Jadson Pereira <95438480+jadsow@users.noreply.github.com>
2023-01-10 14:13:17 -0300

    Add files via upload

---
+++ Modulo-01/src/script.js
@@ -0,0 +1,14 @@
+document.querySelector('.app').innerHTML = `
+    <ul id="paiLista">
+        <li> Hello World </li>
+        <li> Hello World </li>
+        <li id="terceiroFilho"> Hello World </li>
+        <li id="quartoFilho"> Hello World </li>
+        <li id="quintoFilho"> Hello World </li>
+        <li id="sextoFilho"> Hello World </li>
+        <li> Hello World </li>
+        <li id="oitavoFilho"> Hello World </li>
+        <li id="nonoFilho"> Hello World </li>
+        <li id="decimoFilho" > Hello World </li>
+    </ul> 
+`
